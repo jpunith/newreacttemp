0f4ed0152e57a99859f8d07c0a1e545a06a03d94
punith jain <punith.jain1@gmail.com>
2025-02-12 13:35:28 +0530

    logoupdate

--- src/containers/LeftSidebar.js
+++ src/containers/LeftSidebar.js
@@ -24,7 +24,7 @@ function LeftSidebar() {
                 </button>
 
                 <li className="mb-2 font-semibold text-xl">
-                    <Link to={'/app/welcome'}><img className="mask mask-squircle w-10" src="/logo192.png" alt="DashWind Logo" />DashWind</Link> </li>
+                    <Link to={'/app/welcome'}><img className="w-64" src="/logo-tredence.svg" alt="DashWind Logo" /></Link> </li>
                 {
                     routes.map((route, k) => {
                         return (
